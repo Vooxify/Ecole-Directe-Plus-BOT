f491856cdc86f63592d4793b0132c563c5eb65e3
name <leopaulchutell@gmail.com>
2024-05-25 10:39:37 +0200

    Remove all discord bot from the "Documentation" branch

--- src/commands/misc/hello.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'hello',
-    description: "Say hello to be polite ! :-)",
-    callback: ( client, interaction ) => {
-        interaction.reply("Hello, or Hi ;-)")
-    }
-}
