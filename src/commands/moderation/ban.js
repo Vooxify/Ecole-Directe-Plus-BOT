a644e57378303891724bb00a1fa7431c06662594
name <leopaulchutell@gmail.com>
2024-05-25 10:35:59 +0200

    Add the start of the documentation.

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,7 +1,7 @@
 const { ApplicationCommandOptionType, PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-    deleted: true,
+    deleted: false,
     name: 'ban',
     description: 'ban somewone bahahha',
     options: [
